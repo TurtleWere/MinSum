4f7433dff5e92cfa0f3ffc82d7a55ce46d481f4e
TurtleWere <wereturtle68@gmail.com>
2025-05-06 00:11:44 +0300

    Addes array type cheking and corresponding test/ Update README.md

--- Program.cs
+++ Program.cs
@@ -4,12 +4,19 @@ namespace MinSum
 {
     public class MinSum
     {
-        public static int SumTwoSmallestNumbers(int[] numbers)
+        public static int SumTwoSmallestNumbers(object[] objects)
         {
-            if (numbers == null || numbers.Length < 2)
+            if (objects == null || objects.Length < 2)
             {
                 throw new ArgumentException("Массив должен включать в себя 2 и более элентов.");
             }
+            if(objects.Any(n => !(n is int)))
+            {
+                throw new ArgumentException("Массив должен состоять из чисел.");
+            }
+
+            int[] numbers = Array.ConvertAll(objects, x => (int)x);
+
             int firstMin = int.MaxValue;
             int secondMin = int.MaxValue;
 
@@ -39,17 +46,18 @@ namespace MinSum
             Test3();
             Test4();
             Test5();
+            Test6();
         }
 
         private static void Test1()
         {
-            int[] numbers = { 4, 0, 3, 19, 492, -10, 1 };
+            object[] numbers = { 4, 0, 3, 19, 492, -10, 1 };
             var result = MinSum.SumTwoSmallestNumbers(numbers);
             Console.WriteLine($"Тест 1: {(result == -10 ? "Выполнено" : "Провалено")} ");
         }
         private static void Test2()
         {
-            int[] numbers = { 4, 4, 4, 4, 4 };
+            object[] numbers = { 4, 4, 4, 4, 4 };
             var result = MinSum.SumTwoSmallestNumbers(numbers);
             Console.WriteLine($"Тест 2: {(result == 8 ? "Выполнено" : "Провалено")} ");
 
@@ -58,7 +66,7 @@ namespace MinSum
         {
             try
             {
-                var numbers = Array.Empty<int>();
+                var numbers = Array.Empty<object>();
                 var result = MinSum.SumTwoSmallestNumbers(numbers);
                 Console.WriteLine($"Тест 3: Провалено(ошибка не поймана)");
             }
@@ -70,7 +78,7 @@ namespace MinSum
 
         private static void Test4()
         {
-            var numbers = new int[1000000];
+            var numbers = new object[1000000];
             var random = new Random();
             for ( int i = 0; i < numbers.Length; i++ )
             {
@@ -86,13 +94,26 @@ namespace MinSum
         {
             try
             {
-                int[] numbers = { 1 };
+                object[] numbers = { 1 };
                 var result = MinSum.SumTwoSmallestNumbers(numbers);
                 Console.WriteLine($"Тест 5: Провалено(ошибка не поймана)");
             }
             catch (ArgumentException)
             {
                 Console.WriteLine($"Тест 5: Выполнено(ошибка поймана)");
+            }
+        }
+        private static void Test6()
+        {
+            try
+            {
+                object[] notNumbers = { 1, "cat", 5, "spoon", -7, 0.1 };
+                var result = MinSum.SumTwoSmallestNumbers(notNumbers);
+                Console.WriteLine($"Тест 6: Провалено(ошибка не поймана)");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine($"Тест 6: Выполнено(ошибка поймана)");
             }
         }
     }
